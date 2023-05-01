f0a3ffe86653b2beaad718e6f6d293ff05bfe527
Olga Dobrydina <doa1981@mail.ru>
2023-05-01 20:45:30 +0500

    MassNambers

---
+++ HW_47/HW_47.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк массива ");
+int rownum = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива ");
+int columnnum = int.Parse(Console.ReadLine());
+double[,] mass = MassNumbers(rownum, columnnum);
+PrintMass(mass);
+
+
+
+double[,] MassNumbers(int row, int column)
+{
+    double[,] array = new double[row, column];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            array[i, j] = new Random().Next(-8, 16);
+    return array;        
+}
+
+void PrintMass(double[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    for(int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {array[i, j]} ");
+        Console.WriteLine();    
+    }
+    Console.WriteLine();      
+}
