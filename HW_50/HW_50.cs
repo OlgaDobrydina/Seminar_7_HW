3c0ac427e21dece711f74e0eeeecbcbc6036e1d5
Olga Dobrydina <doa1981@mail.ru>
2023-05-03 20:39:17 +0500

    Index

--- HW_50/HW_50.cs
+++ HW_50/HW_50.cs
@@ -7,55 +7,55 @@
 // 17 -> такого числа в массиве нет
 
 Console.Write("Введите количество строк массива ");
-int rownum = int.Parse(Console.ReadLine());
+int row = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов массива ");
+int column = int.Parse(Console.ReadLine());
+Console.Write("Введите номер строки ");
+int rownum = int.Parse(Console.ReadLine());
+Console.Write("Введите номер столбца ");
 int columnnum = int.Parse(Console.ReadLine());
-Console.Write("Введите искомое число ");
-int number = int.Parse(Console.ReadLine());
-int[,] mass = MassNumbers(rownum, columnnum);
+int[,] mass = MassNumbers(row, column);
 PrintMass(mass);
-PrintNumbers(mass, number);
+PrintNumbers(mass, rownum, columnnum);
 
 
 int[,] MassNumbers(int row, int column)
 {
-    
+
     int[,] array = new int[row, column];
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
             array[i, j] = new Random().Next(1, 10);
-    return array;        
+    return array;
 }
 
 void PrintMass(int[,] array)
 {
     int row = array.GetLength(0);
     int column = array.GetLength(1);
-    for(int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < column; j++)
-            Console.Write($" {array[i, j]} ");
-        Console.WriteLine();    
+            Console.Write($"{array[i, j],4}");
+        Console.WriteLine();
     }
-    Console.WriteLine();      
+    Console.WriteLine();
 }
 
-void PrintNumbers(int[,] array, int number)
+void PrintNumbers(int[,] array, int rownum, int columnnum)
 {
-	    int row = array.GetLength(0);
-	    int column = array.GetLength(1);
-	
-	    for (int i = 0; i < row; i++)
-	    {
-	        for (int j = 0; j < column; j++)
-	        {
-	            if (number == array[i, j])
-	            {
-	                Console.WriteLine($"{i + 1}, {j + 1}");
-	                return;
-	            }
-	        }
-	    }
-	    Console.WriteLine("Нет такого числа в массиве");
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    int a = rownum;
+    int b = columnnum;
+    int i = 0;
+    int j = 0;
+
+    if (a > row | b > column) Console.WriteLine("Нет");
+    else 
+    {
+        i = a; j = b;
+        Console.WriteLine(array[i-1,j-1]);
+    }
     
 }
